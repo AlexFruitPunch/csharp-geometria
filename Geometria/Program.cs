30abc15dda346cae052a9fc973cad15b1d46e71d
alexpagani <alexpaganibr@gmail.com>
2022-04-07 14:51:26 +0200

    Creazione dell'oggetto delle prime classi da inizializzare

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿//Utilizzo le mie classi e i metodi del NAMESPACE "Geometria"
+using Geometria;
+
+//Caricamento degli imput da parte dell'utente
+Console.Write("Inserisci un valore per la base: ");
+int Base = int.Parse(Console.ReadLine());
+
+Console.Write("Inserisci un valore per la l'altezza: ");
+int altezza = int.Parse(Console.ReadLine());
+
+
+//Creo il mio oggetto e lo inizializzo
+Rettangolo mioRettangolo = new Rettangolo(Base, altezza);
+
+//Stampo il contenuto del mio oggetto
+mioRettangolo.StampaRettangolo();
+
+
---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Rettangolo
+    {
+        private int baseRettangolo;
+        private int altezzaRettangolo;
+
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+
+        public void StampaRettangolo()
+        {
+            Console.WriteLine("La base del mio rettangolo è: " + baseRettangolo);
+            Console.WriteLine("l'altezza del mio rettangolo è: " + altezzaRettangolo);
+        }
+    }
+}
+
+    
